9a97e4b7e4902ee0831a77db097a4c7492861d7a
marcosZaragoza <marcos.zaragoza7302@gmail.com>
2022-12-01 20:07:04 -0300

    agrego estilo

--- src/app/Components/candela/candela.component.ts
+++ src/app/Components/candela/candela.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, importProvidersFrom, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-candela',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CandelaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private titulo:Title) {
+    titulo.setTitle('Hamburgueseria')
+  }
 
   ngOnInit(): void {
   }
